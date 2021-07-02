4724a2d0a268cbf8b61ea28bf07ec331d8bec183
Bekmirzo1 <azimovbekmirzo0@gmail.com>
2021-07-02 15:47:59 +0500

    some significant modifications

--- js/script.js
+++ js/script.js
@@ -28,29 +28,6 @@ if (iconMenu) {
     })
 }
 ;
-const isMobile = {
-    Android: function () {
-        return navigator.userAgent.match(/Android/i);
-    },
-    BlackBerry: function () {
-        return navigator.userAgent.match(/BlackBerry/i);
-    },
-    iOS: function () {
-        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-    },
-    Opera: function () {
-        return navigator.userAgent.match(/Opera Mini/i);
-    },
-    Windows: function () {
-        return navigator.userAgent.match(/IEMobile/i);
-    },
-    any: function () {
-        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-    }
-};
-if (!isMobile.any()) {
-    document.body.classList.add('_pc')
-};
 const selects = document.querySelectorAll('.select');
 for (let index = 0; index < selects.length; index++) {
     const select = selects[index];
